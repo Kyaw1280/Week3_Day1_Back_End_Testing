8ce3fe72090d6b9b5adcdaf780a4ace26e92b52f
Academy <Academy@LAF-C02XT55QJK77.local>
2025-04-08 10:37:44 +0100

    Add Cors

--- app.js
+++ app.js
@@ -1,4 +1,5 @@
 const express = require('express')
+const cors = require('cors')
 const app = express()
 const fruits = require('./routes/fruits')
 
@@ -7,6 +8,7 @@ app.get('/',(req, res) => {
     res.send("Hello Fruity")
 })
 
+app.user(cors())
 app.use(express.json())
 app.use('/fruits', fruits)
 
